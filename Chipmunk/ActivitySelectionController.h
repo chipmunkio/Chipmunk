16291da56adc07605ca39734f58c0d07565b82b3
Amadou Crookes <ASCrookes@Gmail.com>
2013-03-21 00:31:19 -0400

    Redesigned 2nd page to show image and part of article
    when venues are available a map will show instead of the webview

---
+++ Chipmunk/ActivitySelectionController.h
@@ -0,0 +1,13 @@
+//
+//  ActivitySelectionController.h
+//  Chipmunk
+//
+//  Created by Amadou Crookes on 3/20/13.
+//  Copyright (c) 2013 Gabe Jacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivitySelectionController : UIViewController
+
+@end
